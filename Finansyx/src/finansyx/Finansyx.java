573b19dd43e34af506a31e5490ec3e6860b7ce60
Estuardo Díaz <esjoando555@gmail.com>
2013-09-22 18:52:02 -0600

    Nuevo: Inicio del proyecto

---
+++ Finansyx/src/finansyx/Finansyx.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finansyx;
+
+/**
+ *
+ * @author t4r0
+ */
+public class Finansyx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
